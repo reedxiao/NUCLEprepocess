67cabb6a1d46dd0e083da488154dab345e3f2c83
Kiarie <kiarie.ndegwa@gmail.com>
2016-09-18 19:32:48 +1000

    Slight change

--- src/BeausoupTester.py
+++ src/BeausoupTester.py
@@ -1,3 +1,11 @@
+'''
+@author: Kiarie Ndegwa
+        Sept 18th 2016
+
+Run this file to generate data sets that can be used by a seq2seq network
+
+Change the foldername and filename of .sgml file to extract sentences in the required format
+'''
 import BSoupExtract
 foldername = "NUCLE2013"
 fileName = "official.sgml"
@@ -18,9 +26,6 @@ DTarg = NUCLE.dictGen(cTarg)
 #Save dictionaries to text files
 NUCLE.savetoFile(Dorig, "src.dict", None, False)
 NUCLE.savetoFile(DTarg, "tar.dict", None, False)
-#Save evaluation data sets
-#NUCLE.evalGen("../"+foldername+"/"+"OrigText.txt", "src")
-#NUCLE.evalGen("../"+foldername+"/"+"TargetText.txt", "tar")
 
-#Save evaiauation functions
+#Generate and Save evaluation functions
 NUCLE.LengthCheckedEval()
