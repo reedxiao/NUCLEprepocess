3587fb443a223fafd26d6d77ecaf3dd4156751b8
Kiarie <kiarie.ndegwa@gmail.com>
2016-09-21 20:20:05 +1000

    hdf5 files added

--- HDF5WordEmbeddings/src/hdf5wordembeddingTest.py
+++ HDF5WordEmbeddings/src/hdf5wordembeddingTest.py
@@ -11,17 +11,17 @@ foldername = "../../NUCLEPreprocess/NUCLE2013"
 
 hdf5 = hdf5wordembedding.hdf5wordembedding(src=foldername+"/nucle2013.src.dict",
                                            targ= foldername+"/nucle2013.targ.dict",
-                                           word_embeddings="../../../glove.6B/glove.6B.100d.txt",
+                                           word_embeddings="../../../glove.6B/glove.6B.200d.txt",
                                            filename = "nucle2013.hdf5", 
                                            foldername= "../../../wordEmbedding",
-                                           embed_dim = 50)
+                                           embed_dim = 200)
                 
 #Generate test benchmark
-hdf5.GenExp("../../../glove.6B/glove.6B.50d.txt", "nucle2013")
+hdf5.GenExp("../../../glove.6B/glove.6B.200d.txt", "nucle2013_200d")
 #Read in .dict files
 
 #Generate hdf5 files #fingers crossed
-hdf5.savehdf5("nucle2013", "src", 50)
+hdf5.savehdf5("nucle2013_200d", "src", 200)
 
 #Test hdf5 file generated
-hdf5.readhdf5("nucle2013_enc_")
+hdf5.readhdf5("nucle2013_200d_enc_")
